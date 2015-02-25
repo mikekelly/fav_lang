88bedbe59f80313bd9abebe738904ec0db3ac58f
Mike Kelly <mikekelly321@gmail.com>
2015-02-25 17:30:39 +0000

    spike Api interface

--- lib/lang_fav.rb
+++ lib/lang_fav.rb
@@ -1,15 +1,43 @@
 module LangFav
   class CommandLine
-    def initialize(out:, arguments:)
-      @out, @arguments = out, arguments
+    def initialize(out:, arguments:, api: Api.new)
+      @out, @arguments, @api = out, arguments, api
     end
 
     def run
-      out.call "Ruby"
+      out.call target_user.favourite_language
     end
 
     private
 
-    attr_reader :out, :arguments
+    def target_user
+      @target_user ||= api.fetch_user(username)
+    end
+
+    def username
+      arguments.first
+    end
+
+    attr_reader :out, :arguments, :api
+  end
+
+  class Api
+    def fetch_user(username)
+      User.new(username: username)
+    end
+
+    class User
+      def initialize(username:)
+        @username = username
+      end
+
+      def favourite_language
+        "Ruby"
+      end
+
+      private
+
+      attr_reader :username
+    end
   end
 end
